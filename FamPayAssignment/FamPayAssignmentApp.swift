65d2097ea1e2018df1ffe6685fa33c30c92ebc7d
Arnav Singhal <88189594+Arnav-arw@users.noreply.github.com>
2024-03-19 11:36:29 +0530

    Initial Commit

---
+++ FamPayAssignment/FamPayAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamPayAssignmentApp.swift
+//  FamPayAssignment
+//
+//  Created by Arnav Singhal on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FamPayAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
